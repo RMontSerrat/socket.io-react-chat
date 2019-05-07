77ebc465dba01bab3c1c33b2fa5fc0be370ec932
Romulo Mont Serrat <romulomontserrat@gmail.com>
2019-05-07 00:44:00 -0300

    new tests; port in envvar

--- server/app.js
+++ server/app.js
@@ -1,56 +1,66 @@
 const express = require('express');
-const port = 9010;
+const port = process.env.PORT;
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
-const messages = require('./utils/messages');
+const getMessage = require('./utils/messages');
 
-const users = [];
+const rooms = [];
+const initialParams = {
+  users: [],
+  messages: [],
+}
 
 io.sockets.on('connection', function (socket) {
-  socket.on('room', function(room) {
+  socket.on('room', function(roomId) {
     const now = new Date();
-    socket.join(room);
-    io.sockets.in(room).emit('create-room', room);
+    socket.join(roomId);
+    io.sockets.in(roomId).emit('create-room', roomId);
+    if (!rooms[roomId]) {
+      rooms[roomId] = initialParams;
+    };
+
+    const messages = rooms[roomId].messages;
+    const users = rooms[roomId].users;
 
     let userName;
     socket.on('try-login', function (name) {
       if (users.indexOf(name) > -1) {
-        return io.sockets.in(room).emit('login-failed', messages('login-failed'));
+        return io.sockets.in(roomId).emit('login-failed', getMessage('login-failed'));
       }
       userName = name;
       users.push(userName);
       const msgObj = {
-        system: true, msg: messages('login-success', { userName }), date: now };
-  
-      io.sockets.in(room).emit('msg', msgObj);
-      io.sockets.in(room).emit('all-users', users);
-      io.sockets.in(room).emit('login-success', userName);
-  
-      io.sockets.in(room).emit('add-user', userName);
+        system: true, msg: getMessage('login-success', { userName }), date: now };
+
+      messages.push(msgObj);
+      io.sockets.in(roomId).emit('msg', msgObj);
+      io.sockets.in(roomId).emit('all-users', users);
+      io.sockets.in(roomId).emit('all-messages', messages);
+      io.sockets.in(roomId).emit('login-success', userName);
     });
   
     socket.on('msg', function (msg) {
       const msgObj = { userName, msg, date: now };
-  
-      io.sockets.in(room).emit('msg', msgObj);
+      messages.push(msgObj);
+      io.sockets.in(roomId).emit('msg', msgObj);
     });
   
     socket.on('disconnect', function () {
       users.splice(users.indexOf(userName), 1);
   
-      io.sockets.in(room).emit('user-disconnect', userName);
+      io.sockets.in(roomId).emit('user-disconnect', userName);
   
       const msgObj = {
         system: true,
-        msg: messages('user-disconnect', { userName }), date: now
+        msg: getMessage('user-disconnect', { userName }), date: now
       };
-  
-      io.sockets.in(room).emit('msg', msgObj);
+      messages.push(msgObj);
+      io.sockets.in(roomId).emit('msg', msgObj);
     });
   });
 });
 
 server.listen(port, function(){
-  console.log("Rodando o server!");
+  console.log("Rodando o server na porta", port);
 });
--- server/app.test.js
+++ server/app.test.js
@@ -1,65 +1,94 @@
 const io = require('socket.io-client');
+const port = process.env.PORT;
 const server = require('./app');
+const getMessage = require('./utils/messages');
 
 const options = {
   transports: ['websocket'],
   'force new connection': true
 };
 
-const socketURL = 'http://localhost:9010';
+const socketURL = `http://localhost:${port}`;
 
 const chatUser1 = 'jorge';
 const chatUser2 = 'romulo';
 const chatUser3 = 'ze';
-const room1 = 'room 1';
-const room2 = 'room 2';
 
 describe('Chat Events', function(){
   it('Should broadcast new user once they connect', function(done){
     const client = io.connect(socketURL, options);
-
+    const room = 'room 1';
+    
     client.on('connect',function(socket) {
-      client.emit('room', room1);
+      client.emit('room', room);
       client.emit('try-login', chatUser1);
+      client.on('msg', function(message) {
+        expect(message.msg).toEqual(getMessage('login-success', { userName: chatUser1 }));
+      })
     });
  
-    client.on('create-room',function(room){
-      expect(room).toEqual(room1);
+    client.on('create-room',function(currentRoom){
+      expect(currentRoom).toEqual(room);
     });
 
-    client.on('add-user',function(user){
-      expect(user).toEqual(chatUser1);
+    client.on('all-users',function(user){
+      expect(user).toEqual([chatUser1]);
       client.disconnect();
       done(); 
     });
   });
 
-  it('Should broadcast new user to all users', function(done){
+  it('Should broadcast login failed when user is already in use', function(done){
     const client1 = io.connect(socketURL, options);
+    const room = 'room2';
 
     client1.on('connect', function(data){
-      client1.emit('room', room1);
+      client1.emit('room', room);
       client1.emit('try-login', chatUser1);
       const client2 = io.connect(socketURL, options);
 
       client2.on('connect', function(){
-        client2.emit('room', room1);
+        client2.emit('room', room);
+        client2.emit('try-login', chatUser1);
+      });
+      client2.on('login-failed', function(message){
+        expect(message).toEqual(getMessage('login-failed'));
+      }); 
+    });
+    client1.on('all-users', function(user){
+      expect(user).toEqual([chatUser1]);
+      client1.disconnect();
+      done();
+    });
+  });
+
+  it('Should broadcast new user to all users', function(done){
+    const client1 = io.connect(socketURL, options);
+    const room = 'room3';
+    client1.on('connect', function(data){
+      client1.emit('room', room);
+      client1.emit('try-login', chatUser1);
+      const client2 = io.connect(socketURL, options);
+
+      client2.on('connect', function(){
+        client2.emit('room', room);
         client2.emit('try-login', chatUser2);
       });
 
-      client2.on('add-user',function(user){
-        expect(user).toEqual(chatUser2);
+      client2.on('all-users',function(user){
+        expect(user).toEqual([chatUser1, chatUser2]);
         client2.disconnect();
-      });
+        done();
+      }); 
     });
 
     let numUsers = 0;
-    client1.on('add-user', function(user){
+    client1.on('all-users', function(users){
       numUsers += 1;
       if (numUsers === 2) {
-        expect(user).toEqual(chatUser2);
+        console.log('users', users);
+        expect(users).toEqual([chatUser1, chatUser2]);
         client1.disconnect();
-        done();
       }
     });
   });
@@ -67,6 +96,7 @@ describe('Chat Events', function(){
   it('Should be able to broadcast messages', function(done){
     let client1, client2, client3;
     const message = 'Hello World';
+    const room = 'room4';
     let messages = 0;
 
     const checkMessage = function(client){
@@ -84,25 +114,27 @@ describe('Chat Events', function(){
     checkMessage(client1);
 
     client1.on('connect', function() {
-      client1.emit('room', room1);
+      client1.emit('room', room);
       client2 = io.connect(socketURL, options);
       checkMessage(client2);
 
       client2.on('connect', function() {
-        client2.emit('room', room1);
+        client2.emit('room', room);
         client3 = io.connect(socketURL, options);
         checkMessage(client3);
 
         client3.on('connect', function() {
-          client3.emit('room', room1);
+          client3.emit('room', room);
           client2.emit('msg', message);
         });
       });
     });
   });
 
-  it('Should login user in different rooms', function(done){
+  it('Should login users in different rooms', function(done){
     const client1 = io.connect(socketURL, options);
+    const room1 = 'room5';
+    const room2 = 'room6';
 
     client1.on('connect', function(data){
       client1.emit('room', room1);
@@ -122,10 +154,10 @@ describe('Chat Events', function(){
     });
 
     let numUsers = 0;
-    client1.on('all-users', function(user){
+    client1.on('all-users', function(users){
       numUsers += 1;
       if (numUsers === 2) {
-        expect(user).toEqual([chatUser1, chatUser2]);
+        expect(users).toEqual([chatUser1, chatUser2]);
         client1.disconnect();
         done();
       }
