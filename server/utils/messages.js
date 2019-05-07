553cd2e87dee461fa71f70f5ea4caa5281ae1410
Romulo Mont Serrat <romulomontserrat@gmail.com>
2019-05-07 00:44:34 -0300

    refactor messages name

--- server/utils/messages.js
+++ server/utils/messages.js
@@ -1,7 +1,7 @@
 const messages = (message, options = {}) => {
   const msgObj = {
-    'failed-login': 'Ja existe esse nickName nessa sala.',
-    'success-login': `${options.userName} entrou na sala`,
+    'login-failed': 'Ja existe esse nickName nessa sala.',
+    'login-success': `${options.userName} entrou na sala`,
     'user-disconnect': `${options.userName} saiu da sala... :(`
   }
 
