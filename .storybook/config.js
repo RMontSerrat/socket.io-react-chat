8b79a26e61470486912ebcd6a91d0050f4720e0a
Romulo Mont Serrat <romulomontserrat@gmail.com>
2019-05-06 23:12:34 -0300

    update config storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,9 +5,7 @@ import theme from '../src/config/theme';
 addDecorator(withThemesProvider([theme]));
 
 function loadStories() {
-  require('../src/stories/index.stories');
-
-  const req = require.context('../src/stories', true, /\.stories\.js$/);
+  const req = require.context('../src', true, /\.stories\.js$/);
   req.keys().forEach(filename => req(filename));
 }
 
